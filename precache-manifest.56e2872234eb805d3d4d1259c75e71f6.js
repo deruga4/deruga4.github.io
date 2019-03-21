8cd76e88f123420790a8c9ef3216733c1473fe7e
deruga04 <derekli.3628@gmail.com>
2019-03-21 11:53:12 -0400

    Initial commit

---
+++ precache-manifest.56e2872234eb805d3d4d1259c75e71f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eacd04aee13c572de3db",
+    "url": "/static/js/main.550a742a.chunk.js"
+  },
+  {
+    "revision": "c0ed075ab1540edcc802",
+    "url": "/static/js/2.cf1142f0.chunk.js"
+  },
+  {
+    "revision": "eacd04aee13c572de3db",
+    "url": "/static/css/main.e5e86127.chunk.css"
+  },
+  {
+    "revision": "ab6599fba3747720ee4917b9672e4228",
+    "url": "/index.html"
+  }
+];
